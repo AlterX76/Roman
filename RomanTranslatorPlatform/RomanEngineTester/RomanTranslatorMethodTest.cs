5b22844edba5c7acab6c1eaf9d4aa0b0127da625
Gianni <AlterX76@users.noreply.github.com>
2017-08-24 10:58:13 +0200

    Added extra test cases

--- RomanTranslatorPlatform/RomanEngineTester/RomanTranslatorMethodTest.cs
+++ RomanTranslatorPlatform/RomanEngineTester/RomanTranslatorMethodTest.cs
@@ -52,12 +52,17 @@ namespace RomanEngineTester
         [TestMethod]
         public void SpecialNumbersTestUnit()
         {
+            Assert.IsTrue(_romanTranslator.Execute(1) == "I", "Not a valid translation!"); // TDD: works
+            Assert.IsTrue(_romanTranslator.Execute(2) == "II", "Not a valid translation!"); // TDD: works
+            Assert.IsTrue(_romanTranslator.Execute(3) == "III", "Not a valid translation!"); // TDD: works
             Assert.IsTrue(_romanTranslator.Execute(4) == "IV", "Not a valid translation!"); // TDD: works
+            Assert.IsTrue(_romanTranslator.Execute(5) == "V", "Not a valid translation!"); // TDD: works
             Assert.IsTrue(_romanTranslator.Execute(18) == "XVIII", "Not a valid translation!"); // TDD: works
             Assert.IsTrue(_romanTranslator.Execute(19) == "XIX", "Not a valid translation!"); // TDD: works
             Assert.IsTrue(_romanTranslator.Execute(27) == "XXVII", "Not a valid translation!"); // TDD: ?
             Assert.IsTrue(_romanTranslator.Execute(28) == "XXVIII", "Not a valid translation!"); // TDD: ?
             Assert.IsTrue(_romanTranslator.Execute(29) == "XXIX", "Not a valid translation!"); // TDD: ?
+            Assert.IsTrue(_romanTranslator.Execute(30) == "XXX", "Not a valid translation!"); // TDD: ?
             Assert.IsTrue(_romanTranslator.Execute(38) == "XXXVIII", "Not a valid translation!"); // TDD: ?
             Assert.IsTrue(_romanTranslator.Execute(98) == "XCVIII", "Not a valid translation!"); // TDD: ?
         }
